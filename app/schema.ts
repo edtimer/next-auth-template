626738c03bd5d039a337dbebb3430be74da7c218
sundaray <pgcim14.hemant@spjimr.org>
2024-12-03 16:24:11 +0530

    signin schema updated

--- app/schema.ts
+++ app/schema.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
-export const signinSchema = z.object({
+export const signInSchema = z.object({
   email: z
     .string({ required_error: "Email is required" })
     .min(1, "Email is required")
     .email("Invalid email"),
-  password: z.string(),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(1, "Password is required"),
 });
