3217699a63148af4dad11c83e4d9ae2707888cf8
sundaray <pgcim14.hemant@spjimr.org>
2024-12-03 12:08:33 +0530

    client side validation is working

--- components/signin-form.tsx
+++ components/signin-form.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useActionState } from "react";
+
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -8,22 +12,29 @@ import { parseWithZod } from "@conform-to/zod";
 import { signinSchema } from "@/app/schema";
 
 export function SigninForm() {
+  const [lastResult, action] = useActionState(signin, undefined);
+
   const [form, fields] = useForm({
+    // Sync the result of the last submission
+    lastResult,
+
     //Reuse the validation logic on the client
     onValidate({ formData }) {
       return parseWithZod(formData, { schema: signinSchema });
     },
   });
   return (
-    <form action={signin}>
+    <form id={form.id} onSubmit={form.onSubmit} action={action} noValidate>
       <div className="grid gap-6">
         <div className="grid gap-1">
-          <Label htmlFor="email">Email</Label>
-          <Input id="email" type="email" name="email" />
+          <Label>Email</Label>
+          <Input type="email" name={fields.email.name} />
+          <div>{fields.email.errors}</div>
         </div>
         <div className="grid gap-1">
-          <Label htmlFor="password">Password</Label>
-          <Input id="password" type="password" name="password" />
+          <Label>Password</Label>
+          <Input type="password" name={fields.password.name} />
+          <div>{fields.password.errors}</div>
         </div>
         <Button>Sign in</Button>
       </div>
