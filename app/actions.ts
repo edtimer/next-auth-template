cdfc1c4c2a6b8169cd991933e5e91ed543a707f2
sundaray <pgcim14.hemant@spjimr.org>
2024-12-03 11:46:48 +0530

    added conform

---
+++ app/actions.ts
@@ -0,0 +1,11 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { parseWithZod } from "@conform-to/zod";
+import { signinSchema } from "@/app/schema";
+
+export async function signin(prevState: unknown, formData: FormData) {
+  const submission = parseWithZod(formData, {
+    schema: signinSchema,
+  });
+}
--- components/signin-form.tsx
+++ components/signin-form.tsx
@@ -2,17 +2,28 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
+import { signin } from "@/app/actions";
+import { useForm } from "@conform-to/react";
+import { parseWithZod } from "@conform-to/zod";
+import { signinSchema } from "@/app/schema";
+
 export function SigninForm() {
+  const [form, fields] = useForm({
+    //Reuse the validation logic on the client
+    onValidate({ formData }) {
+      return parseWithZod(formData, { schema: signinSchema });
+    },
+  });
   return (
-    <form>
+    <form action={signin}>
       <div className="grid gap-6">
         <div className="grid gap-1">
           <Label htmlFor="email">Email</Label>
-          <Input id="email" name="email" />
+          <Input id="email" type="email" name="email" />
         </div>
         <div className="grid gap-1">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" name="password" />
+          <Input id="password" type="password" name="password" />
         </div>
         <Button>Sign in</Button>
       </div>
