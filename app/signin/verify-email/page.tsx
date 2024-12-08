b85daf20682dd3897733d3495cba461799f418c7
sundaray <pgcim14.hemant@spjimr.org>
2024-12-08 21:38:28 +0530

    verify email direction is working

--- app/signin/verify-email/page.tsx
+++ app/signin/verify-email/page.tsx
@@ -1,3 +1,15 @@
-export function VerifyEmailpage() {
-  return <h1>Verify Email</h1>;
+export default function VerifyEmailPage() {
+  return (
+    <div className="text-center max-w-xl mx-auto p-6">
+      <h1 className="text-2xl font-bold mb-6">Verify Your Email</h1>
+      <p className="text-gray-700 leading-relaxed">
+        We've sent an email verification link to your email address. Please
+        click the verification link in the email. The link will expire in 24
+        hours.
+      </p>
+      <p className="text-gray-700 mt-4 text-sm">
+        Can't find the email? Be sure to check your spam or junk folder.
+      </p>
+    </div>
+  );
 }
