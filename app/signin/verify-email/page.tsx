39fba89f1986068a8794c644f0a57102a2dc7bc6
sundaray <pgcim14.hemant@spjimr.org>
2024-12-08 22:08:04 +0530

    email redirection is working

--- app/signin/verify-email/page.tsx
+++ app/signin/verify-email/page.tsx
@@ -1,15 +1,19 @@
+import Link from "next/link";
+
 export default function VerifyEmailPage() {
   return (
-    <div className="text-center max-w-xl mx-auto p-6">
-      <h1 className="text-2xl font-bold mb-6">Verify Your Email</h1>
+    <div className="text-center max-w-lg mx-auto p-6">
+      <h1 className="text-2xl font-bold mt-12 mb-6">Verify Your Email</h1>
       <p className="text-gray-700 leading-relaxed">
-        We've sent an email verification link to your email address. Please
-        click the verification link in the email. The link will expire in 24
-        hours.
+        We've sent a verification link to your email address. Click the link to
+        verify your account. The link will expire in 24 hours.
       </p>
-      <p className="text-gray-700 mt-4 text-sm">
-        Can't find the email? Be sure to check your spam or junk folder.
+      <p className="text-pretty text-gray-700 my-4 text-sm">
+        Didn't receive the email? Check your spam or junk folder.
       </p>
+      <Link href="/signin" className="mt-6">
+        Back to Sign in
+      </Link>
     </div>
   );
 }
