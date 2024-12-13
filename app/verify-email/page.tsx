160dac39d190c845d7a8c9ef79c9370468e66748
sundaray <pgcim14.hemant@spjimr.org>
2024-12-13 22:00:25 +0530

    error message extracted

--- app/verify-email/page.tsx
+++ app/verify-email/page.tsx
@@ -1,31 +1,8 @@
-import { verifyCredentialEmail } from "@/lib/verify-credential-email";
-
 import Link from "next/link";
 import { Icons } from "@/components/icons";
+import { ErrorMessage } from "@/lib/error-message";
+import { verifyCredentialEmail } from "@/lib/verify-credential-email";
 import { VerifyCredentialEmailError } from "@/lib/verify-credential-email-error";
-
-export function ErrorMessage({
-  code,
-}: {
-  code: keyof typeof VerifyCredentialEmailError.errorMessages;
-}) {
-  const message = VerifyCredentialEmailError.getErrorMessage(code);
-  return (
-    <div className="mx-auto max-w-sm mt-12 px-4 lg:px-8 text-center">
-      <h2 className="text-red-600 text-2xl font-bold">
-        Token Verification Error
-      </h2>
-      <p className="text-gray-700 my-4">{message}</p>
-      <Link
-        href="/signin"
-        className="flex justify-center items-center group p-2"
-      >
-        <Icons.arrowLeft className="size-4 inline-block mr-2 text-muted-foreground transform transition-transform group-hover:-translate-x-1 group-hover:text-primary" />
-        Sign In
-      </Link>
-    </div>
-  );
-}
 
 export default async function VerifyEmailPage({
   searchParams,
