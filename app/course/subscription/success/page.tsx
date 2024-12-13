4c031216f31b3362a5a27b5bf40ea24fab08adf3
sundaray <pgcim14.hemant@spjimr.org>
2024-12-13 16:55:57 +0530

    subscription pages created

---
+++ app/course/subscription/success/page.tsx
@@ -0,0 +1,10 @@
+export default function SubscriptionSuccessPage() {
+  return (
+    <div className="mx-auto max-w-sm mt-12 px-4 lg:px-8 text-center">
+      <h2 className="text-green-600 text-2xl font-bold">Subscribed</h2>
+      <p className="text-gray-700 my-4">
+        I'll send you an email when the course is launched.
+      </p>
+    </div>
+  );
+}
