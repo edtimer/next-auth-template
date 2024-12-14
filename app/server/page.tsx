8d08d507a57cd73bdc225a58e562ca06c0aab318
sundaray <pgcim14.hemant@spjimr.org>
2024-12-14 11:39:08 +0530

    added server page

--- app/server/page.tsx
+++ app/server/page.tsx
@@ -1,3 +1,26 @@
-export function ServerPage() {
-  return <p>This is a Server Component.</p>;
+import { auth } from "@/auth";
+
+export default async function ServerPage() {
+  const session = await auth();
+
+  if (!session) {
+    return (
+      <div className="mt-12 text-center">
+        <p className="text-sm">User email: Not available</p>
+        <p className="text-sm">User role: Not available</p>
+        <p className="mt-4 text-gray-700 font-medium">
+          This is a Server Component.
+        </p>
+      </div>
+    );
+  }
+  return (
+    <div className="mt-12 text-center">
+      <p className="text-sm">{session.user.email}</p>
+      <p className="text-sm">{session.user.role}</p>
+      <p className="mt-4 text-gray-700 font-medium">
+        This is a Server Component.
+      </p>
+    </div>
+  );
 }
