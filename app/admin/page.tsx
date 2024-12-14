32c431674f9fd1bf8c9639a7655be2bba98b6ee0
sundaray <pgcim14.hemant@spjimr.org>
2024-12-14 12:25:09 +0530

    fixed tailwind class

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -8,7 +8,7 @@ export default async function AdminPage() {
 
   if (role === "user") {
     return (
-      <div className="ma-auto max-w-lg mt-12 text-center">
+      <div className="mx-auto max-w-lg mt-12 text-center">
         <p className="font-medium text-red-600">Admin Access Only</p>
         <p className="mt-4 text-gray-700 font-medium text-pretty">
           This page is only accessible to authenticated users having
