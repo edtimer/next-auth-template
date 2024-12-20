8eeee380c5965ed0a3feecf2734523962ec36135
sundaray <pgcim14.hemant@spjimr.org>
2024-12-20 14:23:18 +0530

    sign out component refactored

---
+++ components/sign-out.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { handleSignOut } from "@/app/signin-actions";
+import { Icons } from "@/components/icons";
+
+export function SignOut() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleClick = async () => {
+    try {
+      setIsLoading(true);
+      await handleSignOut();
+    } catch (error) {
+      console.error("Unable to sign out:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Button
+      disabled={isLoading}
+      variant="ghost"
+      size="sm"
+      onClick={handleClick}
+    >
+      {isLoading ? (
+        <>
+          <Icons.loader className="size-3 animate-spin text-muted-foreground" />
+          Sign out
+        </>
+      ) : (
+        <>
+          <Icons.logOut className="size-3 text-muted-foreground" />
+          Sign out
+        </>
+      )}
+    </Button>
+  );
+}
